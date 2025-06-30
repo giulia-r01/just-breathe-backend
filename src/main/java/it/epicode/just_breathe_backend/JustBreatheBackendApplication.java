e957e9456661c19bd6ad3e1901fedad0ddf35df6
Giulia Rizzo <girzzo@gmail.com>
2025-06-30 15:00:03 +0200

    Initial commit

---
+++ src/main/java/it/epicode/just_breathe_backend/JustBreatheBackendApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.just_breathe_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustBreatheBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustBreatheBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/just_breathe_backend/JustBreatheBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.just_breathe_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustBreatheBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
