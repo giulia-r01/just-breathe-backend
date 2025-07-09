91368684848bf5a5b76398f1c340e95d91d85395
Giulia Rizzo <girzzo@gmail.com>
2025-07-09 12:19:13 +0200

    modifica securityConfig per aggiunta patch

--- src/main/java/it/epicode/just_breathe_backend/security/SecurityConfig.java
+++ src/main/java/it/epicode/just_breathe_backend/security/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
-        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("Authorization", "Content-Type")); // importante!
         corsConfiguration.setAllowCredentials(true);
 
